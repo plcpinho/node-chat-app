246925e473494afc6536603e41e2083975fb8133
Paulo Coronado <plcpinho@gmail.com>
2020-04-24 18:46:24 -0300

    intent filter added

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,13 @@
 window.addEventListener('load', e => {
     if ('serviceWorker' in navigator) {
-        try {
-            navigator.serviceWorker.register('js/sw.js');
-            console.log('SW registered');
-        } catch (error) {
-            console.log('SW failed');
-        }
-    }
+        window.addEventListener('load', () => {
+          navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+              console.log(`Service Worker registered! Scope: ${registration.scope}`);
+            })
+            .catch(err => {
+              console.log(`Service Worker registration failed: ${err}`);
+            });
+        });
+      }
 });
--- public/js/sw.js
+++ public/js/sw.js
@@ -1,13 +1,11 @@
 // NOTE Using WorkBox from Google to simplify the Service Worker code
-importScripts('./node_modules/workbox-sw/build/workbox-sw.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
 
-const staticAssets = [
-  '../',
-  './app.js',
-  './chat.js',
-  '../css/styles.min.css'
-];
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
 
-const wb = new WorkboxSW();
+  workbox.precaching.precacheAndRoute([]);
 
-wb.precache(staticAssets);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
